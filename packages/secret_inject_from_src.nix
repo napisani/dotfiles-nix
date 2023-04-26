ce64d5d115468e6a477a80f84fa3a85022d23ddd
Nick Pisani <napisani@yahoo.com>
2023-04-26 11:05:36 -0400

    nix

--- packages/secret_inject_from_src.nix
+++ packages/secret_inject_from_src.nix
@@ -2,10 +2,12 @@
 stdenv.mkDerivation {
   name = "secret_inject";
   version = "0.1.1";
-  src = fetchurl { 
-     url = "https://github.com/napisani/secret_inject/releases/download/v0.1.1/secret_inject-v0.1.1-x86_64-apple-darwin.tar.xz";
-     hash = "sha256-F4RaJnt2+g4jKlhWfk1W3IUuyUVqV+Cz/U97ItAPDUU=";
-  };
+  src = builtins.fetchFromGithub({
+    owner = "napisani";
+    repo = "secret_inject";
+    rev = "13a1f3fcd2ca1a773c62d1ffa74f7b02bb419479";
+  });
+
   buildCommand = ''
     cargo build --release
     cp target/release/secret_inject $out/bin/secret_inject
