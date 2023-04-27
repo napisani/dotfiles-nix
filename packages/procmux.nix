84be40ad07aee3a4a19fb98a89caacafac0ab6fb
Nick Pisani <napisani@yahoo.com>
2023-04-27 09:45:50 -0400

    fixing brew

--- packages/procmux.nix
+++ packages/procmux.nix
@@ -1,8 +1,33 @@
 { stdenv, lib, pkgs }:
-pkgs.python3Packages.buildPythonApplication rec {
+let 
+
+  ptterm = pkgs.python39Packages.buildPythonApplication rec {
+  version = "0.2";
+  pname = "ptterm";
+  src = pkgs.python39Packages.fetchPypi {
+    inherit pname version;
+    hash = "sha256-pPhG+/XZ8wKlRCyGB9KbMb6rCK23KrNGBQhafFXrEXo=";
+  };
+  /* src = pkgs.fetchFromGitHub({ */
+  /*   owner = "napisani"; */
+  /*   repo = "procmux"; */
+  /*   rev = "ceb93ffa551b91ff19d4eb95bfbb030d300ef973"; */
+  /*   hash = "sha256-Uerv3Dg84jAHhySh86OZi7mgOaS3SWwWzNQcatymvqc="; */
+  /* }); */
+  doCheck = false;
+  pythonImportCheck = [ "ptterm" ];
+  nativeBuildInputs = [ 
+    pkgs.python39Packages.setuptools 
+    pkgs.python39Packages.prompt_toolkit
+    pkgs.python39Packages.pyte
+    pkgs.python39Packages.six
+  ];
+};
+in
+pkgs.python39Packages.buildPythonApplication rec {
   version = "1.0.10";
   pname = "procmux";
-  src = pkgs.python3Packages.fetchPypi {
+  src = pkgs.python39Packages.fetchPypi {
     inherit pname version;
     hash = "sha256-IHKCfi+L4BnTzr/KjT+ZacS8oGoSI9rQIytNbxff040=";
   };
@@ -15,17 +40,17 @@ pkgs.python3Packages.buildPythonApplication rec {
   doCheck = false;
   pythonImportCheck = [ "procmux" ];
   nativeBuildInputs = [ 
-    pkgs.python3Packages.setuptools 
-    /* pkgs.python3Packages.jinja2 */
-    /* pkgs.python3Packages.attrs */
-    /* pkgs.python3Packages.pyparsing */
-    /* pkgs.python3Packages.six */
-    /* pkgs.python3Packages.pyte */
-    /* pkgs.python3Packages.py */
-    /* pkgs.python3Packages.tomli */
-    /* pkgs.python3Packages.packaging */
-    /* pkgs.python3Packages.hiyapyco */
-    /* pkgs.python3Packages.pluggy */
-    /* pkgs.python3Packages.pluggy */
+    pkgs.python39Packages.setuptools 
+    pkgs.python39Packages.jinja2
+    pkgs.python39Packages.attrs
+    pkgs.python39Packages.pyparsing
+    pkgs.python39Packages.six
+    pkgs.python39Packages.pyte
+    pkgs.python39Packages.py
+    pkgs.python39Packages.tomli
+    pkgs.python39Packages.packaging
+    pkgs.python39Packages.hiyapyco
+    pkgs.python39Packages.pluggy
+    ptterm
   ];
 }
