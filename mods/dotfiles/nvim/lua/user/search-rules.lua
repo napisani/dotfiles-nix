1585fdcec5b5db8af6c0c70ad0085d4b81a4efd1
Nick Pisani <napisani@yahoo.com>
2023-04-28 21:57:49 -0400

    nvim

--- mods/dotfiles/nvim/lua/user/search-rules.lua
+++ mods/dotfiles/nvim/lua/user/search-rules.lua
@@ -1,9 +1,5 @@
-local status_ok, search_rules = pcall(require, "nvim-search-rules")
-if not status_ok then
-  vim.notify("nvim-search-rules not found ")
-  return
-end
--- search_rules.setup({})
--- local rooter = require("user.nvim-rooter")
--- globs = search_rules.get_ignore_globs({'.gitignore', '.nvimignore'}, rooter.get_root_dir())
--- require('user.utils').print(globs)
+-- local status_ok, search_rules = pcall(require, "nvim-search-rules")
+-- if not status_ok then
+--   vim.notify("nvim-search-rules not found ")
+--   return
+-- end
