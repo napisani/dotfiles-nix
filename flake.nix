057a7cc58a539280188c6a34eb0984e4bbd3d3ee
Nick Pisani <napisani@yahoo.com>
2023-05-19 08:40:02 -0400

    adding new computer

--- flake.nix
+++ flake.nix
@@ -23,51 +23,49 @@
     procmux.inputs.nixpkgs.follows = "nixpkgs";
 
   };
- outputs = { nixpkgs, nixpkgs-unstable,  home-manager, darwin, procmux,  ... }@inputs: {
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, darwin, procmux, ... }@inputs: {
     darwinConfigurations = {
       "nick-macbook-small" = inputs.darwin.lib.darwinSystem {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          system = "aarch64-darwin";
-          modules = [
-            ./systems/darwin.nix
-            home-manager.darwinModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = false;
-                useUserPackages = true;
-                extraSpecialArgs = { 
-                  inherit inputs; 
-                  pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin;
-                  procmux = procmux;
-                };
-                users.nick.imports = [ ./homes/macs.nix ];
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        system = "aarch64-darwin";
+        modules = [
+          ./systems/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = false;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                inherit inputs;
+                pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin;
+                procmux = procmux;
               };
-            }
-            /* home-manager.darwinModules.home-manager */
-            /* { */
-            /*   home-manager = { */
-            /*     useGlobalPkgs = true; */
-            /*     useUserPackages = true; */
-            /*     /1* extraSpecialArgs = { inherit pwnvim; }; *1/ */
-            /*     pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance *1/ */
-            /*     extraSpecialArgs = { */ 
-            /*       inherit inputs; */ 
-            /*       pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin; */
-            /*     }; # Pass flake inputs to our config */
-            /*     users.nick.imports = [ */ 
-            /*       ./systems/macs.nix */ 
-            /*     ]; */
-            /*   }; */
-            /* } */
-          ];
-          /* modules = [ */
-          /*   inputs.home-manager.darwinModules.home-manager */
-          /*   /1* ./nix/modules/shared *1/ */
-          /*   /1* sharedHostsConfig *1/ */
-          /*   ./mods/darwin.nix */
-          /* ]; */
-        };
+              users.nick.imports = [ ./homes/macs.nix ];
+            };
+          }
+        ];
       };
+      "Nicks-MacBook-Pro" = inputs.darwin.lib.darwinSystem {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        system = "aarch64-darwin";
+        modules = [
+          ./systems/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = false;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                inherit inputs;
+                pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin;
+                procmux = procmux;
+              };
+              users.nick.imports = [ ./homes/macs.nix ];
+            };
+          }
+        ];
+      };
+    };
     defaultPackage = {
       /* x86_64-darwin = home-manager.defaultPackage.x86_64-darwin; */
       aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
@@ -76,53 +74,53 @@
     /* homeConfigurations = { */
     /*   "nick@nick-macbook-small.local" = home-manager.lib.homeManagerConfiguration { */
     /*     pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance */
-    /*     extraSpecialArgs = { */ 
-    /*       inherit inputs; */ 
+    /*     extraSpecialArgs = { */
+    /*       inherit inputs; */
     /*       pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin; */
     /*     }; # Pass flake inputs to our config */
     /*     # > Our main home-manager configuration file < */
-    /*     modules = [ */ 
+    /*     modules = [ */
     /*       # ./systems/darwin.nix */
-    /*       ./systems/macs.nix */ 
+    /*       ./systems/macs.nix */
     /*     ]; */
-    /*   }; */    
+    /*   }; */
     /* }; */
   };
 
 }
-    /* defaultPackage = { */
-    /*   "x86_64-darwin" =  home-manager.defaultPackage.x86_64-darwin; */
-    /*   "aarch64-darwin" = home-manager.defaultPackage.aarch64-darwin; */
-    /* }; */
-   /* darwinConfigurations."nick@nick-macbook-small.local" = darwin.lib.darwinSystem { */
-   /*    system = "aarch64-darwin"; */
-   /*    modules = [ ./mods/darwin.nix ]; */
-   /*    specialArgs = { inherit home-manager; }; */
-   /*  }; */
-    /* darwinConfiguration."nick@nick-macbook-small.local" = darwin.lib.darwinSystem { */
-    /*   system = "aarch64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac */
-    /*   pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance */
-    /*   extraSpecialArgs = { */ 
-    /*     inherit inputs; */ 
-    /*     pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin; */
-    /*   }; # Pass flake inputs to our config */
-    /*   modules = [ */
-    /*     home-manager.darwinModules.home-manager */
-    /*     ./systems/macs.nix */ 
-    /*   ]; # will be important later */
-    /* }; */
-    /* homeConfigurations."nick@MacBook-Pro.local" = home-manager.lib.homeManagerConfiguration { */
-    /*     pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance */
-    /*     extraSpecialArgs = { */ 
-    /*       inherit inputs; */ 
-    /*         pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin; */
-    /*     }; # Pass flake inputs to our config */
-    /*     # > Our main home-manager configuration file < */
-    /*     modules = [ */ 
-    /*       # ./systems/darwin.nix */
-    /*       ./systems/macs.nix */ 
-    /*       home-manager.darwinModules.home-manager */
-    /*     ]; */
-    /*   }; */    
-    /* }; */
+/* defaultPackage = { */
+/*   "x86_64-darwin" =  home-manager.defaultPackage.x86_64-darwin; */
+/*   "aarch64-darwin" = home-manager.defaultPackage.aarch64-darwin; */
+/* }; */
+/* darwinConfigurations."nick@nick-macbook-small.local" = darwin.lib.darwinSystem { */
+/*    system = "aarch64-darwin"; */
+/*    modules = [ ./mods/darwin.nix ]; */
+/*    specialArgs = { inherit home-manager; }; */
+/*  }; */
+/* darwinConfiguration."nick@nick-macbook-small.local" = darwin.lib.darwinSystem { */
+/*   system = "aarch64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac */
+/*   pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance */
+/*   extraSpecialArgs = { */
+/*     inherit inputs; */
+/*     pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin; */
+/*   }; # Pass flake inputs to our config */
+/*   modules = [ */
+/*     home-manager.darwinModules.home-manager */
+/*     ./systems/macs.nix */
+/*   ]; # will be important later */
+/* }; */
+/* homeConfigurations."nick@MacBook-Pro.local" = home-manager.lib.homeManagerConfiguration { */
+/*     pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance */
+/*     extraSpecialArgs = { */
+/*       inherit inputs; */
+/*         pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin; */
+/*     }; # Pass flake inputs to our config */
+/*     # > Our main home-manager configuration file < */
+/*     modules = [ */
+/*       # ./systems/darwin.nix */
+/*       ./systems/macs.nix */
+/*       home-manager.darwinModules.home-manager */
+/*     ]; */
+/*   }; */
+/* }; */
 /* } */
