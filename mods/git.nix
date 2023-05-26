7f6932f2e24d88f6b8e15ba71f63d2267d84d555
Nick Pisani <napisani@yahoo.com>
2023-05-26 09:16:49 -0400

    adding dev/prod branch diff keybindings

--- mods/git.nix
+++ mods/git.nix
@@ -5,6 +5,9 @@
     userEmail = "napisani@yahoo.com";
     ignores = [ "*~" ".DS_Store" ".direnv" ".env.local" ".env" ".rgignore" ];
     extraConfig = {
+      core = {
+        editor = "nvim";
+      };
       init = { defaultBranch = "main"; };
     };
   };
