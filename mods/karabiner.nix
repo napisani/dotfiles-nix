32747653be6896290c2946ad7b4f7a047d38894f
Nick Pisani <napisani@yahoo.com>
2023-07-04 10:37:00 -0400

    simlayers

--- mods/karabiner.nix
+++ mods/karabiner.nix
@@ -1,3 +1,8 @@
-{
-  home.file.".config/karabiner/karabiner.json".source = ./dotfiles/karabiner.json;
+{ pkgs, pkgs-unstable, config, ... }: {
+  /* home.file.".config/karabiner/karabiner.json".source = ./dotfiles/karabiner.json; */
+  xdg.configFile = {
+    "karabiner/karabiner.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/mods/dotfiles/karabiner.json";
+    };
+  };
 }
