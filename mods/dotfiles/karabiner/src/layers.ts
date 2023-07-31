0e6c56b02fd51d3215be0a6a90d8d9e9b2a947ee
Nick Pisani <napisani@yahoo.com>
2023-07-31 08:02:23 -0400

    key binding delimiter layer 2

--- mods/dotfiles/karabiner/src/layers.ts
+++ mods/dotfiles/karabiner/src/layers.ts
@@ -4,8 +4,8 @@ export const layerRules = [
 
   // Delimiter layer - special characters that are hard-ish to reach / remember
   //     ('")
-  //   a  [{,.}]
-  //      <&  *>
+  //   a [{,.}]
+  //     <&  *>
   //
   simlayer('a', 'delimiters layer').manipulators([
     map('r').to({ key_code: '9', modifiers: ['left_shift']}), // (
@@ -24,23 +24,40 @@ export const layerRules = [
     map('n').to({ key_code: '8', modifiers: ['left_shift']}), // *
   ]),
   
+  // Delimiter layer - special characters that are hard-ish to reach / remember
+  //   $ +   -
+  //      :/?=  ;
+  //     \ ^  |
+  // 
+  simlayer('l', 'delimiters layer 2').manipulators([
+    map('r').to({ key_code: 'equal_sign', modifiers: ['left_shift']}), // +
+    map('u').to({ key_code: 'hyphen' }), // -
+    map('f').to({ key_code: 'semicolon', modifiers: ['left_shift']}),  // :
+    map('j').to({ key_code: 'equal_sign' }), // =
+    map('g').to({ key_code: 'slash'}), // /
+    map('h').to({ key_code: 'slash', modifiers: ['left_shift']}), // ?
+    map('c').to({ key_code: 'backslash'}), // \
+    map('m').to({ key_code: 'backslash', modifiers: ['left_shift']}), //  |
+    map('e').to({ key_code: '4', modifiers: ['left_shift']}), // $
+    map('b').to({ key_code: '6', modifiers: ['left_shift']}), // ^
+  ]),
 
   // Number layer 
-  //    4 5 6  
-  //   0 1 2 3 
-  //      7 8 9 
+  //   1 2 3 4  
+  //   5 6 7 8 
+  //       9 0 
 
   simlayer('n', 'number layer').manipulators([
-    map('s').to('0'),
-    map('d').to('1'),
-    map('f').to('2'),
-    map('g').to('3'),
-    map('e').to('4'),
-    map('r').to('5'),
-    map('t').to('6'),
-    map('c').to('7'),
-    map('v').to('8'),
-    map('b').to('9'),
+    map('v').to('0'),
+    map('q').to('1'),
+    map('w').to('2'),
+    map('e').to('3'),
+    map('r').to('4'),
+    map('a').to('5'),
+    map('s').to('6'),
+    map('d').to('7'),
+    map('f').to('8'),
+    map('c').to('9'),
   ]),
 
   // simlayer('e', 'emoji layer').manipulators([
