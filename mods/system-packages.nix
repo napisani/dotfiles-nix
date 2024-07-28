b6fb180b367c5f9f80060192fcd49e16551a0529
Nick Pisani <napisani@yahoo.com>
2024-07-28 12:34:59 -0400

    kill kube

--- mods/system-packages.nix
+++ mods/system-packages.nix
@@ -1,9 +1,12 @@
-{ pkgs, pkgs-unstable, procmux, secret_inject, ... }: {
-  environment.systemPackages = with pkgs; [
-    tmux
-    unzip
-    wget
-    kubectl
-  ];
+{ pkgs, pkgs-unstable, procmux, secret_inject, ... }:
+let
+  kubekill = pkgs.writeScriptBin "killkube.sh" ''
+    #!/usr/bin/env bash
+    kubectl delete deployment postgres -n home 
+    kubectl delete deployment pgvector -n home
+    kubectl delete deployment mongo -n home
+  '';
+in {
+  environment.systemPackages = with pkgs; [ tmux unzip wget kubectl kubekill ];
 
 }
